acf3c0f0ed79873f07cacd89db6a1adb3f31268a
Abbas Rzayev <rzabbasqa@gmail.com>
2026-04-14 11:41:22 +0400

    check HomeWork1

--- src/test/java/Lesson_03_Operators/lesson_03_08_Homework.java
+++ src/test/java/Lesson_03_Operators/lesson_03_08_Homework.java
@@ -19,14 +19,25 @@ public class lesson_03_08_Homework {
 
         //Arifmetik tapşırığın cavabı
         int endirimMigdari= mehsulQiymeti*endirimFaizi/100;
-        System.out.println(" Endirim miqdarı: " + endirimMigdari);
-        int qazancHesabla=mehsulQiymeti*qazanc/100;
+        System.out.println("Endirim miqdarı: " + endirimMigdari);
+
+        int qazancHesabla= mehsulQiymeti * qazanc/100;
         System.out.println(" Qazanc hesabı: " + qazancHesabla);
-        int yekunQiymet=mehsulQiymeti+endirimFaizi+qazanc;
+
+        int yekunQiymet=mehsulQiymeti+endirimFaizi+qazanc; // (mehsulunQiymet - endirimFaizi + qazanc)
         System.out.println(" Yekun Qiymət: " + yekunQiymet);
+
         int ucEdedQiymet=yekunQiymet*adet;
         System.out.println(" Üç ədəd üçün ümumi məbləğ: " + ucEdedQiymet);
+
         double tamBolunme=120%7;
+
+        //int qaliq = 120 % 7;   // 1
+        // qaliq 0-dırsa tam bölünür, deyilsə bölünmürgit
+        //System.out.println("Qalıq: " + qaliq);              // 1
+        //System.out.println("Tam bölünür: " + (qaliq == 0)); // false
+
+
         System.out.println(" Tam Bolunme: " + tamBolunme);
         System.out.print("---------------------------------");
         System.out.println("\n Endirim miqdarı: " + endirimMigdari + "\n Qazanc: " + qazancHesabla + "\n Yekun Qiymət: " + yekunQiymet + ("\n Üç ədəd üçün ümumi məbləğ: " + ucEdedQiymet + "\n Tam Bolunme: " + tamBolunme ));
